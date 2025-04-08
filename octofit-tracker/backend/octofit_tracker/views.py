88e1d5111a2d440a806fccd3c04596e52a009834
mhemant <mhemant@gmail.com>
2025-04-08 18:16:01 +0000

    run app

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -7,11 +7,11 @@ from .models import User, Team, Activity, Leaderboard, Workout
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
-        'users': 'api/users/',
-        'teams': 'api/teams/',
-        'activities': 'api/activities/',
-        'leaderboard': 'api/leaderboard/',
-        'workouts': 'api/workouts/'
+        'users': 'https://organic-space-guide-wrv697j9j4529p9p-8000.app.github.dev/api/users/',
+        'teams': 'https://organic-space-guide-wrv697j9j4529p9p-8000.app.github.dev/api/teams/',
+        'activities': 'https://organic-space-guide-wrv697j9j4529p9p-8000.app.github.dev/api/activities/',
+        'leaderboard': 'https://organic-space-guide-wrv697j9j4529p9p-8000.app.github.dev/api/leaderboard/',
+        'workouts': 'https://organic-space-guide-wrv697j9j4529p9p-8000.app.github.dev/api/workouts/'
     })
 
 class UserViewSet(viewsets.ModelViewSet):
